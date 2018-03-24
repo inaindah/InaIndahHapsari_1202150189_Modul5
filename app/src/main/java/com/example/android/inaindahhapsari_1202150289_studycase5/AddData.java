4665eba04713703543f53748f5b98b2f9a424204
DESKTOP-M8QA7VG\ASUS <inaindah24@gmail.com>
2018-03-24 23:18:40 +0700

    InaIndahHapsari_1202150289_Modul5

---
+++ app/src/main/java/com/example/android/inaindahhapsari_1202150289_studycase5/AddData.java
@@ -0,0 +1,42 @@
+package com.example.android.inaindahhapsari_1202150289_studycase5;
+
+/**
+ * Created by ASUS on 24/03/2018.
+ */
+
+public class AddData {
+    //mendeklarasikan variable
+    String todo, desc, prior;
+
+    //konstruktor
+    public AddData(String todo, String desc, String prior){
+        this.todo=todo;
+        this.desc=desc;
+        this.prior=prior;
+    }
+
+    //method setter dan getter untuk to do , description dan priority
+    public String getTodo() {
+        return todo;
+    }
+
+    public void setTodo(String todo) {
+        this.todo = todo;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getPrior() {
+        return prior;
+    }
+
+    public void setPrior(String prior) {
+        this.prior = prior;
+    }
+}
